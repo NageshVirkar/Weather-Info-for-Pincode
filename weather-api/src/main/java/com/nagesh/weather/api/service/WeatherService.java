9ba23464f6466f6b9270e0fe71713d99c1109a00
NageshVirkar <virkarnagesh19@gmail.com>
2024-10-18 23:44:30 +0530

    Revert "First Commit"
    This reverts commit 7d02c24b662f3603e451d0ed0bca64e44a5a1248.

--- weather-api/src/main/java/com/nagesh/weather/api/service/WeatherService.java
+++
@@ -1,7 +0,0 @@
-package com.nagesh.weather.api.service;
-
-import com.nagesh.weather.api.model.WeatherInfo;
-
-public interface WeatherService {
-	WeatherInfo getWeatherForPincode(String pincode, String date);
-}
